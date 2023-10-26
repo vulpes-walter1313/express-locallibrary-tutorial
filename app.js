15c7a0f1aee8069f8d5d4806376b3fa1ea030893
Walter Fritsche <wfritsche95@protonmail.com>
2023-10-26 11:11:12 -0500

    added compression in middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
 require("dotenv").config();
 
 const app = express();
@@ -32,6 +33,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
